a1faeee103eff4c111464808c97a85575ea66371
Musungu2 <ruthmusungu2019@gmail.com>
2025-10-21 15:45:18 +0300

    fixed

--- routes/stringRoutes.js
+++ routes/stringRoutes.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const router = express.Router();
+
+// Import controller functions
 const {
   createString,
   getString,
@@ -7,13 +10,11 @@ const {
   deleteString,
 } = require("../controllers/stringController");
 
-const router = express.Router();
-
+// Define routes
 router.post("/strings", createString);
+router.get("/strings/:string_value", getString);
 router.get("/strings", getAllStrings);
 router.get("/strings/filter-by-natural-language", filterByNaturalLanguage);
-router.get("/strings/:string_value", getString);
 router.delete("/strings/:string_value", deleteString);
 
 module.exports = router;
-
